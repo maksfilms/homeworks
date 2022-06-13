29223e4d75c797adcac07527b817420fc79624b1
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-06-13 22:11:18 +0300

    refactoring hw 5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
 import {PATH} from "./Pages";
+import s from "./pages/Pages.module.css"
 
 function Header() {
     return (
         <div>
-            <NavLink to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
-            <NavLink to={PATH.JUNIOR}>Junior</NavLink>
-            <NavLink to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
+            <NavLink className={s.prejunior} to={PATH.PRE_JUNIOR}><b>Pre-Junior</b></NavLink>
+            <NavLink className={s.junior} to={PATH.JUNIOR}><b>Junior</b></NavLink>
+            <NavLink className={s.juniorplus} to={PATH.JUNIOR_PLUS}><b>Junior+</b></NavLink>
+            <span className={s.magicButton}>Open</span>
         </div>
     )
 }
