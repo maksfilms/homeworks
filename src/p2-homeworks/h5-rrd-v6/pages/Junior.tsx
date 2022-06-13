4d8ecc4a33072c238053052014d341565d9ac0dc
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-06-13 20:18:21 +0300

    homework 5

---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function PreJunior() {
+    return (
+        <div>
+            HW7-HW11
+        </div>
+    )
+}
+
+export default PreJunior
---
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PreJunior() {
+    return (
+        <div>
+            HW12-to the end
+        </div>
+    )
+}
+
+export default PreJunior
